14b8aed38e209dba3a156f0238ed4ea6fffc1d46
Diogo Araújo <36507885+dichico@users.noreply.github.com>
2024-01-16 13:38:46 +0000

    53w53xx

---
+++ testeorbit/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  testeorbit
+//
+//  Created by Diogo Francisco Costa on 16/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ testeorbit/testeorbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testeorbitApp.swift
+//  testeorbit
+//
+//  Created by Diogo Francisco Costa on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct testeorbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
