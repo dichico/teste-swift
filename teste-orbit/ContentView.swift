7a6f47dc87bcc4f5773d95c64221757f07fbcbb0
Diogo Araújo <36507885+dichico@users.noreply.github.com>
2024-01-16 11:37:26 +0000

    adicionar projeto teste

---
+++ teste-orbit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  teste-orbit
+//
+//  Created by Diogo Francisco Costa on 16/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello and welcome to Orbit Actions!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ teste-orbit/teste_orbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  teste_orbitApp.swift
+//  teste-orbit
+//
+//  Created by Diogo Francisco Costa on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct teste_orbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
